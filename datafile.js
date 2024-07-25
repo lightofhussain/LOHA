480f8f2f54e74bbe495495edf1bae8bb5f0e0e6b
lightofhussain <lightofhussain@gmail.com>
2024-07-26 00:46:47 +1000

    Add files via upload

---
+++ datafile.js
@@ -0,0 +1,257 @@
+const aboutData=[
+    {
+        id:0,
+        title:"Aims & Objectives",
+        detail:"<h5>Community Empowerment:-</h5> To empower our community by providing opportunities for economic participation and self-sufficiency. This includes creating jobs, supporting local entrepreneurship, and enhancing community resilience."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Sustainability:-</h5> To promote sustainable practices in business operations, such as environmental responsibility, ethical sourcing, and long-term viability. This ensures that the LOHA contributes positively to both the environment and the community's well-being.To empower our community by providing opportunities for economic participation and self-sufficiency. This includes creating jobs, supporting local entrepreneurship, and enhancing community resilience."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Local Economic Development:-</h5> To stimulate local economic growth and development by keeping financial resources within the community. This may involve reinvesting profits locally, supporting local suppliers and producers, and attracting investment to the community."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Education and Training:-</h5> To provide education and training opportunities for community members, enhancing their skills and capabilities. This could include workshops, mentoring programs, and vocational training that empower individuals to participate effectively in cooperative activities or start their own businesses."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Ethical Business Practices:-</h5> To uphold ethical standards in business practices, such as fair trade, transparency, and accountability. This builds trust within the community and ensures that the LOHA operates with integrity."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Advocacy and Representation:-</h5> To advocate for the interests of the community on local, regional, and sometimes national levels. This includes lobbying for policies that support cooperative enterprises, fair economic practices, and community development."
+    },
+    {
+        id:0,
+        title:"",
+        detail:"<h5>Long-Term Viability:-</h5> To ensure the LOHA's long-term sustainability and success through effective governance, financial management, and strategic planning. This involves setting clear goals, monitoring performance, and adapting to changing circumstances while staying true to cooperative principles."
+    },
+    {
+        id:1,
+        title:"About us",
+        detail:"Light of Hussain Australia (LOHA) established on 17 July 2024 by Engr. Syed Tanweer Hussain  in Australia"
+    },
+    {
+        id:1,
+        title:"First ever business cooperative",
+        detail:"is a shia muslim business cooperative. We welcomes individuals and businesses from shia background only. Who are committed to our cooperative principles and goals."
+    },
+    {
+        id:1,
+        title:"We are trying to ",
+        detail:"Stimulate community economic growth and entrepreneurship. Improve the overall quality of life and financial capacities of shia community by establishing  large business chain with joint effect"
+    },
+
+
+
+    {
+        id:2,
+        title:"Contact us",
+        detail:""
+    },
+    {
+        id:2,
+        title:"Email",
+        detail:"lightofhussain@gmail.com"
+    },
+    {
+        id:2,
+        title:"Phone",
+        detail:" +61 415249942"
+    },
+    {
+        id:2,
+        title:"Address ",
+        detail:"31 Essence Parkway CRAIGIEBURN 3064 VIC AUSTRILA"
+    },
+    {
+        id:3,
+        title:"MISSION STATEMENT",
+        detail:"At LOHA(Light of Hussain Australia) our mission is to create a dynamic space where local businesses thrive, residents connect, and cultural exchange flourishes. Through collaborative initiatives and shared spaces, we aim to enhance economic vitality, promote social interaction, and celebrate diversity, thereby enriching the quality of life for all members of our community."
+    },
+
+    {
+        id:4,
+        title:"OUR GOALS",
+        detail:"Stimulate local economic growth and entrepreneurship."
+    },
+    {
+        id:4,
+        title:"",
+        detail:"Foster social interaction and community cohesion."
+    },
+    {
+        id:4,
+        title:"",
+        detail:"Improve the overall quality of life for community residents."
+    },
+    {
+        id:4,
+        title:"",
+        detail:"Promote environmental sustainability and responsible urban development."
+    },
+    {
+        id:4,
+        title:"",
+        detail:"Foster partnerships with local organizations, businesses, and stakeholders."
+    },
+    {
+        id:4,
+        title:"",
+        detail:"Ensure accessibility and inclusivity for all community members."
+    },
+    {
+    id:5,
+    title:"HELP & SUPPORT",
+    detail:"At this stage we only provid help to our members  in following categories and add more in future."
+},
+{
+    id:5,
+    title:"1. Supportive Network",
+    detail:"Access to a supportive network of like-minded individuals and businesses who share our values and goals."
+},
+{
+    id:5,
+    title:"2. Business Development",
+    detail:"Opportunities for business development, training, and mentorship to help you grow your enterprise."
+},
+{
+    id:5,
+    title:"3. Advocacy and Representation",
+    detail:"We advocate for our member's interests on local and regional levels, ensuring our voice is heard in matters affecting our community."
+},
+{
+    id:6,
+    title:"UP COMING PROJECTS",
+    detail:""
+},
+{
+    id:6,
+    title:"",
+    detail:""
+},
+{
+    id:6,
+    title:"Project.No.5: Buying Commercial Building OR Land",
+    detail:"Currently we are working on the project to buy a commercial building/land. Membership/Applications are open for this project."
+},
+{
+    id:6,
+    title:"",
+    detail:""
+},
+{
+    id:6,
+    title:"Project.No.6: Seven Eleven Franchise ",
+    detail:"Currently we are working on the project to buy a Seven Eleven Franchise. Membership/Applications are open for this project."
+},
+
+{
+    id:7,
+    title:"Light of Hussain Australia Cooperative  (LOHA)",
+    detail:""
+},
+{
+    id:7,
+    title:"",
+    detail:""
+},
+{
+    id:7,
+    title:" ",
+    detail:"LOHA was established in 2024 by Shia Muslims in Australia, led by Engr. Syed Tanweer Hussain and his associates. The cooperative aims to unite community members to achieve economic and social goals for mutual benefit. Key objectives include empowering the Shia community through economic participation and self-sufficiency, fostering job creation, and enhancing community resilience. LOHA also prioritizes sustainable business practices, including environmental responsibility and ethical sourcing, to ensure positive contributions to both the environment and community well-being. By keeping financial resources within the community, LOHA seeks to stimulate local economic growth and ensure long-term sustainability and success."
+}
+
+]
+
+
+const mainPage=[
+    {
+        id:0,
+        title:"	Community Impact",
+        detail:"By joining our cooperative, you become a part of a community-focused initiative that prioritizes the well-being and prosperity of our local community"
+    },
+    {
+        id:0,
+        title:"Economic Empowerment",
+        detail:"We offer opportunities for members to contribute to and benefit from our cooperative's activities, including job creation, business support, and economic resilience."
+    },
+    {
+        id:0,
+        title:"Sustainability",
+        detail:"Our cooperative promotes sustainable practices and ethical business operations, ensuring that our activities positively impact both our environment and community."
+    },    
+    { 
+        id:0,
+        title:"	Supportive Network",
+        detail:"	Access to a supportive network of like-minded individuals and businesses who share your values and goals."},
+    {
+        id:0,
+        title:"	Business Development",
+        detail:"	Opportunities for business development, training, and mentorship to help you grow your enterprise."
+    }, 
+    {
+        id:0,
+        title:"	Advocacy and Representation",
+        detail:"	We advocate for our members' interests on local and regional levels, ensuring your voice is heard in matters affecting our community."
+    },
+  
+
+    {
+        id:1,
+        title:"	Supportive Network",
+        detail:"	Access to a supportive network of like-minded individuals and businesses who share your values and goals. "
+    },
+    {
+        id:1,
+        title:"	Business Development",
+        detail:"	Opportunities for business development, training, and mentorship to help you grow your enterprise. ",
+    },
+    {
+        id:1,
+        title:"	Advocacy and Representation",
+        detail:"	We advocate for our members' interests on local and regional levels, ensuring your voice is heard in matters affecting our community.. "
+    },
+
+    {
+        id:2,
+        title:"	Joining Light of Hussain Australia is straightforward! ",
+        detail:"	"
+    },
+    {
+        id:2,
+        title:"",
+        detail:"	Attend an orientation session,"
+    },
+    {
+        id:2,
+        title:"",
+        detail:"	Fill out an application form,"
+    },
+    {
+        id:2,
+        title:"",
+        detail:"	 Pay membership fee."
+    },
+    {
+        id:2,
+        title:"",
+        detail:"	Our cooperative welcomes individuals and businesses from shia background only. Who are committed to our cooperative principles and goals."
+    },
+ 
+
+
+
+]
+
+
+
+
