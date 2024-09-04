39b221cd0263187987f6db16866e41a61eddf18d
lightofhussain <lightofhussain@gmail.com>
2024-09-04 21:37:06 +1000

    Add files via upload

---
+++ myJavaScript.js
@@ -0,0 +1,23 @@
+let p1=document.getElementById("p")
+
+console.log("javaScript")
+
+function fnButtons(id){
+   if(id===0){
+      // About us
+      window.location = "about.html"
+   }
+   if(id===1){
+      // Join us
+      window.location = "joinUs.html"
+   }
+  
+
+if(id===4){
+   // our plan
+   window.location = "contactUs.html"
+}
+
+
+
+}
